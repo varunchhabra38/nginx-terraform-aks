e5d3d53efef9a3cf7c09ebf034c8713d4a6b45f2
Varun Chhabra <varunchhabra38@gmail.com>
2022-06-08 11:19:05 +0200

    aks terraform

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -65,7 +65,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 # Network Profile
   network_profile {
     network_plugin = "azure"
-    load_balancer_sku = "Standard"
+    load_balancer_sku = "standard"
   }
 
   tags = {
