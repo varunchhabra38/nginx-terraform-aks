e975e2b15926b8ac6d29253fefcb34a5769c2554
Varun Chhabra <varunchhabra38@gmail.com>
2022-06-08 13:17:28 +0200

    aks terraform

--- app-gateway.tf
+++ app-gateway.tf
@@ -35,7 +35,7 @@ resource "azurerm_application_gateway" "network" {
 
   frontend_ip_configuration {
     name                 = local.frontend_ip_configuration_name
-    public_ip_address_id = azurerm_public_ip.test.id
+    public_ip_address_id = azurerm_public_ip.public_ip.id
   }
 
   backend_address_pool {
