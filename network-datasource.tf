b4f08dccee9088144a33c4b7925bca472724255a
Varun Chhabra <varunchhabra38@gmail.com>
2022-06-08 10:35:51 +0200

    aks terraform

--- network-datasource.tf
+++ network-datasource.tf
@@ -2,7 +2,7 @@ data "azurerm_subnet" "kubesubnet" {
   name                 = var.aks_subnet_name
   virtual_network_name = azurerm_virtual_network.aksvnet.name
   resource_group_name  = azurerm_resource_group.aks_rg.name
-  depends_on           = [azurerm_subnet.kubesubnet]
+  depends_on           = [azurerm_subnet.aks_subnet]
 }
 
 data "azurerm_subnet" "appgwsubnet" {
